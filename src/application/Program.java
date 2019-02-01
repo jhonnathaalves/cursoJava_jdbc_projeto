0407db9a9dc05d7ea6b2a2ae2397d06ee923058e
jhonnathaalves <jhonnatha16011987@yahoo.com.br>
2019-02-01 17:11:27 -0200

    Reusing instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,14 +11,11 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "João");
-		Seller seller = new Seller(21,"Bob","bob@gmail.com",new Date(),3000.00, obj);
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		Seller seller2 = sellerDao.findById(3); 
-		
-		System.out.println(seller2);
+		System.out.println("=== TEST 1: seller findById ====");
+		Seller seller = sellerDao.findById(3); 
+		System.out.println(seller);
 		
 
 	}
